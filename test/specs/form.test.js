efaefb0cd62320e7fde37bb23cdb68ea8555ba24
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-08-01 13:16:10 -0700

    added rest of positive test cases

--- test/specs/form.test.js
+++ test/specs/form.test.js
@@ -7,4 +7,52 @@ describe('Testing the', () => {
         await registerPage.posTester(testVals.posEPus);
         await expect(registerPage.step2).toBeDisplayed();
     });
+
+    it('Equivalent Partition Canada', async () => {
+        await registerPage.open();
+        await registerPage.posTester(testVals.posEPca);
+        await expect(registerPage.step2).toBeDisplayed();
+    });
+
+    it('Equivalent Partition UK', async () => {
+        await registerPage.open();
+        await registerPage.posTester(testVals.posEPuk);
+        await expect(registerPage.step2).toBeDisplayed();
+    });
+
+    it('Boundary Value Low US', async () => {
+        await registerPage.open();
+        await registerPage.posTester(testVals.posBVALus);
+        await expect(registerPage.step2).toBeDisplayed();
+    });
+
+    it('Boundary Value Low Canada', async () => {
+        await registerPage.open();
+        await registerPage.posTester(testVals.posBVALca);
+        await expect(registerPage.step2).toBeDisplayed();
+    });
+
+    it('Boundary Value Low UK', async () => {
+        await registerPage.open();
+        await registerPage.posTester(testVals.posBVALuk);
+        await expect(registerPage.step2).toBeDisplayed();
+    });
+
+    it('Boundary Value High US', async () => {
+        await registerPage.open();
+        await registerPage.posTester(testVals.posBVAHus);
+        await expect(registerPage.step2).toBeDisplayed();
+    });
+
+    it('Boundary Value High Canada', async () => {
+        await registerPage.open();
+        await registerPage.posTester(testVals.posBVAHca);
+        await expect(registerPage.step2).toBeDisplayed();
+    });
+
+    it('Boundary Value High UK', async () => {
+        await registerPage.open();
+        await registerPage.posTester(testVals.posBVAHuk);
+        await expect(registerPage.step2).toBeDisplayed();
+    });
 });
