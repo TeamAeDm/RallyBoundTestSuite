51dee1e3274d712ea51807b0e6fc7fa00bec3923
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-08-01 13:35:08 -0700

    bug: posTester always doing first name neg test

--- test/pageobjects/register.page.js
+++ test/pageobjects/register.page.js
@@ -53,7 +53,7 @@ class registerPage extends Page {
         await this.next.click;
     }
 
-    async posTester(posArray){ this.signUp(posArray, 0, 0); }
+    async posTester(posArray){ this.signUp(posArray, 12, 0); }
     async negTester(posArray, negTest, negArray) {
         for (let i = 0; i <= negArray.length; i++) {
             await this.signUp(posArray, negTest, negArray[i]);
