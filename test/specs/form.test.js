352b6f4a70439c2d4669d5fdbfa8fc5e38a22c4b
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-08-01 13:15:42 -0700

    removed debug lines

--- test/specs/form.test.js
+++ test/specs/form.test.js
@@ -3,11 +3,8 @@ import testVals from "./testVals.js";
 
 describe('Testing the', () => {
     it('Equivalent Partition USA', async () => {
-        console.log("Starting the test...");
         await registerPage.open();
-        console.log("Page opened successfully.");
         await registerPage.posTester(testVals.posEPus);
         await expect(registerPage.step2).toBeDisplayed();
-        console.log("Test completed.");
     });
 });
