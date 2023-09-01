3f92fb2f1e4f01197bc259cd2b7115ade2146199
Tazmaniego <DiegoAbrahamMontiel@gmail.com>
2023-08-31 23:23:33 -0700

    added console logs

--- test/pageobjects/register.page.js
+++ test/pageobjects/register.page.js
@@ -56,64 +56,78 @@ async signUp(input) {
     } else {
         console.log(`Expected a string for firstName but received a ${typeof input[0]}`);
     }
+    console.log('input[1] type:', typeof input[1], 'value:', input[1]);
     if (typeof input[1] === 'string') {
         await this.setValueAsKeys(this.lastName, input[1]);
     } else {
         console.log(`Expected a string for LastName but received a ${typeof input[1]}`);
     }
+    console.log('input[2] type:', typeof input[2], 'value:', input[2]);
     if (typeof input[2] === 'string') {
         await this.setValueAsKeys(this.email, input[2]);
     } else {
         console.log(`Expected a string for email but received a ${typeof input[2]}`);
     }
+    console.log('input[3] type:', typeof input[3], 'value:', input[3]);
     if (typeof input[3] === 'string') {
         await this.setValueAsKeys(this.password, input[3]);
     } else {
         console.log(`Expected a string for password but received a ${typeof input[3]}`);
     }
+    console.log('input[4] type:', typeof input[4], 'value:', input[4]);
     if (typeof input[4] === 'string') {
         await this.setValueAsKeys(this.confirmPassword, input[4]);
     } else {
         console.log(`Expected a string for confirmPassword but received a ${typeof input[4]}`);
     }
+    console.log('input[5] type:', typeof input[5], 'value:', input[5]);
     if (typeof input[5] === 'string') {
         await this.setValueAsKeys(this.address, input[5]);
     } else {
         console.log(`Expected a string for address but received a ${typeof input[5]}`);
     }
+    console.log('input[6] type:', typeof input[6], 'value:', input[6]);
     if (typeof input[6] === 'string') {
         await this.setValueAsKeys(this.aptSteUnit, input[6]);
     } else {
         console.log(`Expected a string for aptSteUnit but received a ${typeof input[6]}`);
     }
+    console.log('input[7] type:', typeof input[7], 'value:', input[7]);
     if (typeof input[7] === 'string') {
         await selectByVisibleText(this.country, input[7]);
     } else {
         console.log(`Expected a string for country but received a ${typeof input[7]}`);
     }
+    console.log('input[8] type:', typeof input[8], 'value:', input[8]);
     if (typeof input[8] === 'string') {
         await this.setValueAsKeys(this.city, input[8]);
     } else {
         console.log(`Expected a string for city but received a ${typeof input[8]}`);
     }
+    console.log('input[9] type:', typeof input[9], 'value:', input[9]);
     if (typeof input[9] === 'string') {
         await selectByVisibleText(this.stateProvince, input[9]);
     } else {
         console.log(`Expected a string for stateProvince but received a ${typeof input[9]}`);
     }
+    console.log('input[10] type:', typeof input[10], 'value:', input[10]);
     if (typeof input[10] === 'string') {
         await this.setValueAsKeys(this.zipPostal, input[10]);
     } else {
         console.log(`Expected a string for zipPostal but received a ${typeof input[10]}`);
     }
+    console.log('input[11] type:', typeof input[11], 'value:', input[11]);
     if (typeof input[11] === 'string') {
         await this.phoneNumber(input[11]);
     } else {
         console.log(`Expected a string for phoneNumber but received a ${typeof input[11]}`);
     }
+
     for (let i = 0; i < negArray.length; i++) {
         console.log('Loop iteration:', i);
     }
+}   catch (error) {
+    console.error('An error occurred:', error);
 }
 
     async posTester(posArray, itComment) {
@@ -130,8 +144,8 @@ async signUp(input) {
             expect(await this.testOutcome(true)).toBeTruthy();
         });
     }
-    
-        async negTester (posArray, negTest, negArray=[[],[]], fieldName) {
+
+        async negTester (posArray, negTest, negArray = [["error1", "description1"], ["error2", "description2"]], fieldName) {
         let negFiltered = [];
         for (const elem of posArray) {
             negFiltered.push(elem[0]);
